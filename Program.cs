ffd2d00795379be1ba8865ed5c5fb9095e29084b
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 18:27:55 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("MEVSİM BULUCU");
+  
+  
+Console.Write("Ayı giriniz: ");
+int month = int.Parse(Console.ReadLine());
+  
+  
+if(month <= 2 || month == 12 )
+{
+    Console.WriteLine("Kış");
+}
+
+if (month >= 3 && month <= 5)
+{
+    Console.WriteLine("İlkbahar");
+}
+
+if (month >= 6 && month <= 8)
+{
+    Console.WriteLine("Yaz");
+}
+
+if (month >= 9 && month <= 11)
+{
+    Console.WriteLine("Sonbahar");
+}
